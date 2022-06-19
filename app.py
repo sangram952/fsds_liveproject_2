0a01ba9dfa39213c526c3a2eb4b016e4956b2f62
sangram shinde <sangramshinde952@gmail.com.com>
2022-06-19 18:33:35 +0530

    appy .py file eddirtd

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app=Flask(__name__),
+app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
     return "final testing on 19/6 CICD pipeline setup competed"
   
-if __name__=="_main_":
+if __name__=="__main__":
     app.run(debug=True)
