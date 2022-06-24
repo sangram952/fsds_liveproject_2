1eb96a6f34e004200f21f3cebc1981e68934175b
sangram shinde <sangramshinde952@gmail.com.com>
2022-06-24 12:06:03 +0530

    exception module updated

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from housing.logger import logging
 
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("we are testing logging module")
     return "final testing on 19/6 CICD pipeline setup competed"
   
 if __name__=="__main__":
